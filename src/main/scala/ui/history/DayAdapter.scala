cea0a10254c7a9e6d33db2c7a8a87231acba1450
Dennis Dietrich <d.dietrich84@gmail.com>
2015-03-08 19:33:37 +0100

    break

--- src/main/scala/ui/history/DayAdapter.scala
+++ src/main/scala/ui/history/DayAdapter.scala
@@ -8,6 +8,7 @@ import android.text.Html
 import java.util.ArrayList
 import org.scaloid.common._
 import com.github.nscala_time.time.Imports._
+import java.util.concurrent.TimeUnit
 
 class DayAdapter(
   context: Context,
@@ -22,6 +23,7 @@ class DayAdapter(
 
     val pieces = view.find[TextView](quit.ui.R.id.history_pieces)
     val name = view.find[TextView](quit.ui.R.id.history_day)
+    val break = view.find[TextView](quit.ui.R.id.history_break)
     val day = days.get(position)
 
     pieces.setText(Html.fromHtml(s"<b>${day.dates.length}</b> pcs"))
@@ -29,6 +31,16 @@ class DayAdapter(
       name.setText("Yesterday")
     } else {
       name.setText(day.date.toString("EEE, MMM d"))
+    }
+
+    day.dates.sliding(2).map {
+      x => x(1).getMillis - x(0).getMillis
+    } reduceOption (_ max _) foreach { x =>
+      val h = TimeUnit.MILLISECONDS.toHours(x)
+      val m = TimeUnit.MILLISECONDS.toMinutes(x) -
+              TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(x))
+
+      break.setText(Html.fromHtml(s"<i>Longest break: ${h}h ${m}m</i>"))
     }
 
     view
